83e8ce3e81c706e04ec8a5fd6578e912753aa9f7
jakubgania <contact@jakubgania.io>
2020-08-23 15:49:37 +0200

    adding subpage - /about

---
+++ pages/_lang/about/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    about me
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Jakub Gania Software | O mnie',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'custom description',
+        },
+      ],
+    }
+  },
+}
+</script>
