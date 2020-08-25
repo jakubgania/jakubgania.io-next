ece6d0ccb5c0abf1cd7151a6b1655e4f19f1d2ea
jakubgania <contact@jakubgania.io>
2020-08-26 01:04:24 +0200

    implemented contact subpage

--- pages/_lang/contact/index.vue
+++ pages/_lang/contact/index.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    kontakt
+  <div class="grid grid-cols-1">
+    <div class="content-container">
+      <div>
+        <h2>Kontakt</h2>
+      </div>
+      <ul>
+        <li>Jakub Gania</li>
+        <li>contact@jakubgania.io</li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -15,8 +23,55 @@ export default {
           name: 'description',
           content: 'custom description',
         },
+        {
+          name: 'keywords',
+          contact: 'Kontakt',
+        },
       ],
     }
   },
 }
 </script>
+
+<style lang="scss" scoped>
+.content-container {
+  max-width: 800px;
+  width: 100%;
+  margin: auto;
+}
+.email {
+  padding-top: 80px;
+  text-align: center;
+  color: black;
+  letter-spacing: 2px;
+  font-size: 14px;
+  font-weight: 300;
+}
+.email-link {
+  text-decoration: none;
+  color: #000;
+}
+.email-link:hover {
+  color: #999;
+}
+.email-link-dark-theme {
+  color: #bfbfbf;
+}
+.email-link-dark-theme:hover {
+  color: #fff;
+}
+
+@media only screen and (max-width: 960px) {
+  .content-container {
+    padding-left: 14px;
+    padding-right: 14px;
+  }
+}
+
+@media only screen and (max-width: 600px) {
+  .content-container {
+    padding-left: 14px;
+    padding-right: 14px;
+  }
+}
+</style>
