4eb35b05167e23d043d33f8007edaa3b3a734fb4
jakubgania <contact@jakubgania.io>
2020-08-30 17:28:24 +0200

    implemented gallery subpage

--- pages/_lang/gallery/index.vue
+++ pages/_lang/gallery/index.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    galeria
+  <div class="content-container">
+    <div class="grid grid-cols-3 grid-flow-row gap-1 images-container">
+      <div v-for="index in 12" :key="index" class="image-item"></div>
+    </div>
   </div>
 </template>
 
@@ -20,3 +22,19 @@ export default {
   },
 }
 </script>
+
+<style lang="scss" scoped>
+.content-container {
+  max-width: 1000px;
+  width: 100%;
+  margin: auto;
+}
+.images-container {
+  margin-top: 100px;
+  margin-bottom: 200px;
+}
+.image-item {
+  padding-top: 100%;
+  background-color: #f2f2f2;
+}
+</style>
