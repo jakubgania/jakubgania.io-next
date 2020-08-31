451aac2b6163a0b2d4abc54d0d746992b6a978a9
jakubgania <contact@jakubgania.io>
2020-08-31 22:28:38 +0200

    icon pack installation - @mdi/js

--- pages/_lang/contact/index.vue
+++ pages/_lang/contact/index.vue
@@ -8,12 +8,22 @@
         <li>Jakub Gania</li>
         <li>contact@jakubgania.io</li>
       </ul>
+      <svg style="width: 224px; height: 224px;" viewBox="0 0 24 24">
+        <path :d="mdiEmail" />
+      </svg>
     </div>
   </div>
 </template>
 
 <script>
+import { mdiEmail } from '@mdi/js'
+
 export default {
+  data() {
+    return {
+      mdiEmail,
+    }
+  },
   head() {
     return {
       title: 'Jakub Gania Software | Kontakt',
