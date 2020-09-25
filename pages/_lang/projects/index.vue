3d3d521316ee4b701e4fce6230ed8c510a5f6aca
jakubgania <contact@jakubgania.io>
2020-09-26 00:48:05 +0200

    creating top-image component for the projects subpage

--- pages/_lang/projects/index.vue
+++ pages/_lang/projects/index.vue
@@ -1,26 +1,6 @@
 <template>
   <div>
-    <div class="content-container-next">
-      <div class="top-image-container">
-        <div class="top-image-container__section">
-          <div class="top-image-container__content-section">
-            <div class="top-image-container__title">
-              [ projekty ]
-            </div>
-          </div>
-          <div class="top-image-container__scroll-down-container">
-            <a
-              href="#list-projects"
-              class="top-image-container__scroll-down-anchor"
-            >
-              <div class="top-image-container__cheveron" />
-              <div class="top-image-container__cheveron" />
-              <div class="top-image-container__cheveron" />
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
+    <top-image-component />
     <div class="max-w-xs py-8 mx-auto md:max-w-sm lg:max-w-5xl">
       <div
         class="flex flex-col item-center py-4 lg:gap-8 lg:grid lg:grid-cols-3"
@@ -127,7 +107,9 @@
           </div>
         </div>
       </div>
-      <div>{{ data.viewer.name }}</div>
+      <div>
+        <!-- pp -->
+      </div>
     </div>
     <!-- <div class="list-projects-container">
       <div class="grid grid-rows-1">
@@ -168,6 +150,7 @@
 
 <script>
 import gql from 'graphql-tag'
+import TopImage from '@/components/projects/TopImage'
 
 const githubDataQuery = gql`
   query viewer {
@@ -245,6 +228,9 @@ export default {
       data,
     }
   },
+  components: {
+    'top-image-component': TopImage,
+  },
   data() {
     return {
       githubDataQuery,
@@ -259,8 +245,8 @@ export default {
         'Express',
         'Nodejs',
         'SQL',
-        'Cloud'
-      ]
+        'Cloud',
+      ],
     }
   },
   // apollo: {
@@ -307,105 +293,6 @@ export default {
 .content-container {
   width: 100%;
   margin: 0;
-}
-.top-image-container {
-  position: relative;
-  margin-bottom: 40px;
-
-  $top-image-width: 100%;
-  $top-image-height: 100vh;
-
-  &__section {
-    width: $top-image-width;
-    height: $top-image-height;
-    background-color: #000;
-
-    &::after {
-      content: '';
-      display: block;
-      position: absolute;
-      top: 0;
-      left: 0;
-      background-image: url(../../../assets/images/stickers.jpg);
-      background-position: center;
-      background-size: cover;
-      width: $top-image-width;
-      height: 100vh;
-      opacity: 0.5;
-      background-attachment: fixed;
-    }
-  }
-
-  &__title {
-    text-align: center;
-    color: #fff;
-    text-transform: uppercase;
-    font-size: 10vw;
-    font-weight: 900;
-    letter-spacing: 8px;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-
-  &__content-section {
-    width: 100%;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    z-index: 2;
-  }
-
-  &__scroll-down-container {
-    position: absolute;
-    left: 50%;
-    bottom: calc(0px + 56px);
-    width: 24px;
-    height: 24px;
-  }
-
-  &__scroll-down-anchor {
-    position: absolute;
-    z-index: 4;
-  }
-
-  &__cheveron {
-    position: absolute;
-    width: 28px;
-    height: 8px;
-    opacity: 0;
-    transform: scale3d(0.5, 0.5, 0.5);
-    animation: move 3s ease-out infinite;
-
-    &:first-child {
-      animation: move 3s ease-out 1s infinite;
-    }
-
-    &:nth-child(2) {
-      animation: move 3s ease-out 2s infinite;
-    }
-
-    &::before,
-    &::after {
-      content: ' ';
-      position: absolute;
-      top: 0;
-      height: 100%;
-      width: 51%;
-      background: #fff;
-    }
-
-    &::before {
-      left: 0;
-      transform: skew(0deg, 30deg);
-    }
-
-    &::after {
-      right: 0;
-      width: 50%;
-      transform: skew(0deg, -30deg);
-    }
-  }
 }
 .list-projects-container {
   max-width: 800px;
@@ -503,30 +390,6 @@ export default {
 }
 .post-title {
   font-size: 20px;
-}
-
-@keyframes move {
-  25% {
-    opacity: 1;
-  }
-  33% {
-    opacity: 1;
-    transform: translateY(30px);
-  }
-  67% {
-    opacity: 1;
-    transform: translateY(40px);
-  }
-  100% {
-    opacity: 0;
-    transform: translateY(55px) scale3d(0.5, 0.5, 0.5);
-  }
-}
-
-@keyframes pulse {
-  to {
-    opacity: 1;
-  }
 }
 
 @media only screen and (max-width: 960px) {
