4caa5f5aa8433fd35500f2c022c5332b567ffaec
jakubgania <contact@jakubgania.io>
2020-10-18 23:25:10 +0200

    implemented ProjectsSection component for home subpage

---
+++ components/home/ProjectsSection.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="projects-section">
+    <div class="projects-section-wrapper">
+      <div class="projects-section-title">
+        <h2>
+          Projekty
+        </h2>
+      </div>
+      <div class="projects-sections-container">
+        <div class="projects-item">
+          <ul class="project-list">
+            <li>
+              <nuxt-link :to="$i18n.path('projects/jakubgania-io')">
+                jakubgania.io
+              </nuxt-link>
+            </li>
+            <li>
+              <nuxt-link :to="$i18n.path('projects/python-scripts')">
+                python-scripts
+              </nuxt-link>
+            </li>
+            <li>
+              <nuxt-link :to="$i18n.path('projects/hypercode-it')">
+                hypercode-it
+              </nuxt-link>
+            </li>
+          </ul>
+        </div>
+        <div class="projects-item">
+          <ul class="project-list">
+            <li>
+              <nuxt-link :to="$i18n.path('projects/business-page-project-2')">
+                business-page-project-2
+              </nuxt-link>
+            </li>
+            <li>
+              <nuxt-link
+                :to="
+                  $i18n.path('projects/angular8-omdbapi-movie-search-engine')
+                "
+              >
+                angular8-omdbapi-movie-search-engine
+              </nuxt-link>
+            </li>
+            <li>
+              <nuxt-link :to="$i18n.path('projects/yourcity-frontend')">
+                yourcity-frontend
+              </nuxt-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <more-content-button-component
+        :text="'Więcej projektów'"
+        :link="'projects'"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import MoreContentButtonComponent from '../../components/more-content-button'
+
+export default {
+  comments: {
+    'more-content-button-component': MoreContentButtonComponent,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.projects-section {
+  max-width: 1600px;
+  margin: auto;
+  padding-left: 60px;
+  padding-right: 60px;
+  padding-top: 140px;
+  padding-bottom: 140px;
+}
+.projects-section-wrapper {
+  margin: auto;
+}
+.projects-section-title {
+  font-weight: 800;
+  text-align: center;
+  color: var(--home-page-projects-section-color);
+}
+.projects-sections-container {
+  display: flex;
+}
+.projects-item {
+  width: 50%;
+}
+.projects-item ul {
+  list-style: none;
+  margin-top: 80px;
+}
+.projects-item ul li {
+  margin-bottom: 40px;
+  letter-spacing: 1.2px;
+  color: var(--home-page-projects-section-color);
+}
+.projects-item ul li::before {
+  content: '\2022';
+  color: block;
+  font-weight: bold;
+  display: inline-block;
+  width: 1em;
+  margin-left: -1em;
+  font-size: 20px;
+}
+
+@media only screen and (max-width: 600px) {
+  .projects-section {
+    padding-top: 60px;
+    padding-bottom: 60px;
+  }
+  .projects-sections-container {
+    flex-wrap: wrap;
+  }
+  .projects-section-title h2 {
+    font-size: 36px;
+  }
+  .projects-item {
+    width: 100%;
+  }
+  .projects-item:nth-child(2) ul {
+    margin-top: inherit;
+  }
+}
+</style>
--- pages/_lang/index.vue
+++ pages/_lang/index.vue
@@ -114,63 +114,7 @@
       alt=""
       style="width: 100%;"
     />
-    <div class="projects-section">
-      <div class="projects-section-wrapper">
-        <div class="projects-section-title">
-          <h2>
-            Projekty
-          </h2>
-        </div>
-        <div class="projects-sections-container">
-          <div class="projects-item">
-            <ul class="project-list">
-              <li>
-                <nuxt-link :to="$i18n.path('projects/jakubgania-io')">
-                  jakubgania.io
-                </nuxt-link>
-              </li>
-              <li>
-                <nuxt-link :to="$i18n.path('projects/python-scripts')">
-                  python-scripts
-                </nuxt-link>
-              </li>
-              <li>
-                <nuxt-link :to="$i18n.path('projects/hypercode-it')">
-                  hypercode-it
-                </nuxt-link>
-              </li>
-            </ul>
-          </div>
-          <div class="projects-item">
-            <ul class="project-list">
-              <li>
-                <nuxt-link :to="$i18n.path('projects/business-page-project-2')">
-                  business-page-project-2
-                </nuxt-link>
-              </li>
-              <li>
-                <nuxt-link
-                  :to="
-                    $i18n.path('projects/angular8-omdbapi-movie-search-engine')
-                  "
-                >
-                  angular8-omdbapi-movie-search-engine
-                </nuxt-link>
-              </li>
-              <li>
-                <nuxt-link :to="$i18n.path('projects/yourcity-frontend')">
-                  yourcity-frontend
-                </nuxt-link>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <more-content-button-component
-          :text="'Więcej projektów'"
-          :link="'projects'"
-        />
-      </div>
-    </div>
+    <projects-section-component />
     <social-section-component />
     <images-section-component />
   </div>
@@ -182,7 +126,7 @@ import SocialSectionComponent from '../../components/home/SocialSection'
 import ContentSectionComponent from '../../components/home/ContentSection'
 import ImagesSectionComponent from '../../components/home/ImagesSection'
 import BlogSectionComponent from '../../components/home/BlogSection'
-import MoreContentButtonComponent from '../../components/more-content-button'
+import ProjectsSectionComponent from '../../components/home/ProjectsSection'
 import ImageKeyboardLight from '../../assets/images/keyboard-light.jpg'
 import ImageKeyboardDark from '../../assets/images/keyboard-dark.jpg'
 
@@ -216,7 +160,7 @@ export default {
     'content-section-component': ContentSectionComponent,
     'images-section-component': ImagesSectionComponent,
     'blog-section-component': BlogSectionComponent,
-    'more-content-button-component': MoreContentButtonComponent,
+    'projects-section-component': ProjectsSectionComponent,
   },
   data() {
     return {
@@ -300,7 +244,6 @@ export default {
   transition: letter-spacing 0.2s ease;
   font-size: 14px;
   font-weight: bold;
-  // background-color: #fff;
   background-color: #ffdd1a;
   padding-left: 44px;
   padding-right: 44px;
@@ -410,46 +353,6 @@ export default {
   z-index: 20;
   pointer-events: auto;
 }
-.projects-section {
-  max-width: 1600px;
-  margin: auto;
-  padding-left: 60px;
-  padding-right: 60px;
-  padding-top: 140px;
-  padding-bottom: 140px;
-}
-.projects-section-wrapper {
-  margin: auto;
-}
-.projects-section-title {
-  font-weight: 800;
-  text-align: center;
-  color: var(--home-page-projects-section-color);
-}
-.projects-sections-container {
-  display: flex;
-}
-.projects-item {
-  width: 50%;
-}
-.projects-item ul {
-  list-style: none;
-  margin-top: 80px;
-}
-.projects-item ul li {
-  margin-bottom: 40px;
-  letter-spacing: 1.2px;
-  color: var(--home-page-projects-section-color);
-}
-.projects-item ul li::before {
-  content: '\2022';
-  color: block;
-  font-weight: bold;
-  display: inline-block;
-  width: 1em;
-  margin-left: -1em;
-  font-size: 20px;
-}
 
 @media only screen and (max-width: 960px) {
   .technical-word {
@@ -508,22 +411,6 @@ export default {
   .right-part {
     left: 50%;
     top: 140px;
-  }
-  .projects-section {
-    padding-top: 60px;
-    padding-bottom: 60px;
-  }
-  .projects-sections-container {
-    flex-wrap: wrap;
-  }
-  .projects-section-title h2 {
-    font-size: 36px;
-  }
-  .projects-item {
-    width: 100%;
-  }
-  .projects-item:nth-child(2) ul {
-    margin-top: inherit;
   }
   .social-section-title {
     margin-bottom: 40px;
