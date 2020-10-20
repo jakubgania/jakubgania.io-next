0d518c8772cc204c4c9f4b85a9fe6c239544ec18
jakubgania <contact@jakubgania.io>
2020-10-20 14:49:34 +0200

    update projects subpage

--- pages/_lang/projects/index.vue
+++ pages/_lang/projects/index.vue
@@ -1,186 +1,191 @@
 <template>
   <div class="main-projects-container">
     <top-image-component />
-    <div class="change-button-sect">
-      <div class="button-sect">
-        <div @click="changeView('grid')" class="fgt">
-          widok kafelków
+    <template v-if="!data">
+      <div class="loader">Loading...</div>
+    </template>
+    <template v-else>
+      <div class="change-button-sect">
+        <div class="button-sect">
+          <div @click="changeView('grid')" class="fgt">
+            widok kafelków
+          </div>
+        </div>
+        <div class="button-sect">
+          <div @click="changeView('list')" class="fgt">
+            widok listy
+          </div>
         </div>
       </div>
-      <div class="button-sect">
-        <div @click="changeView('list')" class="fgt">
-          widok listy
+      <div v-show="view == 'list'" class="list-projects-container">
+        <!-- breadcrumb -->
+        <!-- subpage title section component -->
+        <div class="grid grid-rows-1">
+          Opis moich projektów programistycznych
         </div>
-      </div>
-    </div>
-    <div v-show="view == 'list'" class="list-projects-container">
-      <!-- breadcrumb -->
-      <!-- subpage title section component -->
-      <div class="grid grid-rows-1">
-        Opis moich projektów programistycznych
-      </div>
-      <div class="grid grid-rows-1">
-        <ul>
-          <li
-            v-for="project of projects"
-            :key="project.slug"
-            class="post-link-item"
-          >
-            <!-- $i18n.path(project.slug) -->
-            <!-- :to="{ name: 'projects-slug', params: { slug: project.slug } }" -->
-            <!-- :to="$i18n.path('projects/' + project.slug)" -->
-            <!-- "localePath({ name: 'category-slug', params: { slug: category.slug } })" -->
-            <nuxt-link
-              :to="$i18n.path('projects/' + project.slug)"
-              class="post-link"
+        <div class="grid grid-rows-1">
+          <ul>
+            <li
+              v-for="project of projects"
+              :key="project.slug"
+              class="post-link-item"
             >
-              <div class="post-main-title">
-                <div class="post-title">
-                  {{ project.title }}
+              <!-- $i18n.path(project.slug) -->
+              <!-- :to="{ name: 'projects-slug', params: { slug: project.slug } }" -->
+              <!-- :to="$i18n.path('projects/' + project.slug)" -->
+              <!-- "localePath({ name: 'category-slug', params: { slug: category.slug } })" -->
+              <nuxt-link
+                :to="$i18n.path('projects/' + project.slug)"
+                class="post-link"
+              >
+                <div class="post-main-title">
+                  <div class="post-title">
+                    {{ project.title }}
+                  </div>
                 </div>
-              </div>
-            </nuxt-link>
-          </li>
-        </ul>
+              </nuxt-link>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-    <div
-      v-if="view == 'grid' && data"
-      class="max-w-xs py-8 mx-auto md:max-w-sm lg:max-w-5xl"
-    >
       <div
-        class="flex flex-col item-center py-4 lg:gap-2 lg:grid lg:grid-cols-3"
+        v-if="view == 'grid' && data"
+        class="max-w-xs py-8 mx-auto md:max-w-sm lg:max-w-5xl"
       >
         <div
-          class="flex flex-col items-center h-full gap-4 overflow-hidden text-sm bg-gray-10"
+          class="flex flex-col item-center py-4 lg:gap-2 lg:grid lg:grid-cols-3"
         >
-          <div class="flex flex-wrap justify-center p-4 pb-16 bg-gray-20">
-            <template v-for="item in techTags">
-              <tag-component :key="item.id" :name="item" />
-            </template>
-          </div>
-          <!-- <img
-            :src="data.viewer.avatarUrl"
-            alt="My Avatar"
-            class="w-24 -mt-16 rounded-full shadow-xl"
-          /> -->
-          <h2 class="text-xl font-bold">
-            Jakub Gania
-          </h2>
-          <div class="flex flex-col flex-grow px-2 mt-4">
-            <!-- <div v-if="data.viewer.location" class="flex text-gray-70">
-              <div class="flex text-gray-70">
-                <span role="img" aria-label="Graduate Emoji">
-                  🗺️
-                </span>
-                <span class="ml-2">
-                  {{ data.viewer.location }}
-                </span>
-              </div>
-            </div> -->
-          </div>
           <div
-            class="grid w-full grid-cols-3 mt-4 text-xs font-bold uppercase gap-px"
+            class="flex flex-col items-center h-full gap-4 overflow-hidden text-sm bg-gray-10"
           >
-            <a
-              href="https://github.com/jakubgania"
-              target="_blank"
-              rel="noopener noreferrer"
-              class="flex items-center justify-center p-1 font-bold tracking-wide uppercase transition duration-200 border-2 border-transparent bg-gray-70 hover:bg-gray-60 hover:text-white focus:outline-none focus:border-gray-10"
-            >
-              <span class="ml-1 text-white">
-                GitHub
-              </span>
-            </a>
-            <a
-              href="https://www.linkedin.com/in/jakubgania"
-              target="_blank"
-              rel="noopener noreferrer"
-              class="flex items-center justify-center p-1 font-bold tracking-wide uppercase transition duration-200 border-2 border-transparent bg-gray-70 hover:bg-gray-60 hover:text-white focus:outline-none focus:border-gray-10"
-            >
-              <span class="ml-1 text-white">
-                LinkedIn
-              </span>
-            </a>
-            <a
-              href="https://www.youtube.com/channel/UCpRXjQ_FgRfAqP6uIsA7UEQ"
-              target="_blank"
-              rel="noopener noreferrer"
-              class="flex items-center justify-center p-1 font-bold tracking-wide uppercase transition duration-200 border-2 border-transparent bg-gray-70 hover:bg-gray-60 hover:text-white focus:outline-none focus:border-gray-10"
-            >
-              <span class="ml-1 text-white">
-                YouTube
-              </span>
-            </a>
-          </div>
-        </div>
-        <div class="flex flex-col h-full col-span-2 gap-4 mt-4 lg:mt-0">
-          <div class="flex flex-col gap-2">
+            <div class="flex flex-wrap justify-center p-4 pb-16 bg-gray-20">
+              <template v-for="item in techTags">
+                <tag-component :key="item.id" :name="item" />
+              </template>
+            </div>
+            <img
+              :src="data.viewer.avatarUrl"
+              alt="My Avatar"
+              class="w-24 -mt-16 rounded-full shadow-xl"
+            />
             <h2 class="text-xl font-bold">
-              Bio
+              Jakub Gania
             </h2>
-            <div class="flex flex-col h-full gap-4 p-4 bg-gray-10 lg:mt-0">
-              <div class="grid grid-flow-row gap-4">
-                <p>
-                  <span>
-                    <!-- {{ data.viewer.bio }} -->
+            <div class="flex flex-col flex-grow px-2 mt-4">
+              <div v-if="data.viewer.location" class="flex text-gray-70">
+                <div class="flex text-gray-70">
+                  <span role="img" aria-label="Graduate Emoji">
+                    🗺️
                   </span>
-                </p>
-                <p class="text-xs tracking-wide">
-                  I usually put all the projects I create on my Github profile.
-                  Sometimes they are larger applications and sometimes small
-                  test programs.
-                </p>
-                <p class="text-xs tracking-wide">
-                  I learn different technologies and try to use the acquired
-                  knowledge in code.
-                </p>
-                <p class="text-xs tracking-wide">
-                  In addition to programming fullstack applications, I also
-                  create larger or smaller scripts in Bash, Python and
-                  JavaScript.
-                </p>
-                <p class="text-xs tracking-wide">
-                  Currently, I have the most projects in JavaScript, but I also
-                  use popular libraries and frameworks. I learn Vue, React,
-                  Vuetify, Nuxt or Node all the time.
-                </p>
-                <p class="text-xs tracking-wide">
-                  Many projects are not very extensive or finished. I will
-                  highlight the most interesting and extensive ones on this
-                  page.
-                </p>
-                <p class="text-sm">
-                  It is amazing how many possibilities are offered by modern
-                  technologies!
-                </p>
-                <p>
-                  <span role="img" aria-label="inbox emoji" class="mr-1">
-                    📥️
+                  <span class="ml-2">
+                    {{ data.viewer.location }}
                   </span>
-                  DMs are open on
-                  <a
-                    class="transition-colors duration-200 text-blue-50 hover:text-blue-40"
-                    href="https://linkedin.com/in/"
-                  >
-                    LinkedIn
-                  </a>
-                  and
-                  <a
-                    class="transition-colors duration-200 text-blue-50 hover:text-blue-40"
-                    href="https://twitter.com/"
-                  >
-                    Twitter
-                  </a>
-                </p>
+                </div>
+              </div>
+            </div>
+            <div
+              class="grid w-full grid-cols-3 mt-4 text-xs font-bold uppercase gap-px"
+            >
+              <a
+                href="https://github.com/jakubgania"
+                target="_blank"
+                rel="noopener noreferrer"
+                class="flex items-center justify-center p-1 font-bold tracking-wide uppercase transition duration-200 border-2 border-transparent bg-gray-70 hover:bg-gray-60 hover:text-white focus:outline-none focus:border-gray-10"
+              >
+                <span class="ml-1 text-white">
+                  GitHub
+                </span>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/jakubgania"
+                target="_blank"
+                rel="noopener noreferrer"
+                class="flex items-center justify-center p-1 font-bold tracking-wide uppercase transition duration-200 border-2 border-transparent bg-gray-70 hover:bg-gray-60 hover:text-white focus:outline-none focus:border-gray-10"
+              >
+                <span class="ml-1 text-white">
+                  LinkedIn
+                </span>
+              </a>
+              <a
+                href="https://www.youtube.com/channel/UCpRXjQ_FgRfAqP6uIsA7UEQ"
+                target="_blank"
+                rel="noopener noreferrer"
+                class="flex items-center justify-center p-1 font-bold tracking-wide uppercase transition duration-200 border-2 border-transparent bg-gray-70 hover:bg-gray-60 hover:text-white focus:outline-none focus:border-gray-10"
+              >
+                <span class="ml-1 text-white">
+                  YouTube
+                </span>
+              </a>
+            </div>
+          </div>
+          <div class="flex flex-col h-full col-span-2 gap-4 mt-4 lg:mt-0">
+            <div class="flex flex-col gap-2">
+              <h2 class="text-xl font-bold">
+                Bio
+              </h2>
+              <div class="flex flex-col h-full gap-4 p-4 bg-gray-10 lg:mt-0">
+                <div class="grid grid-flow-row gap-4">
+                  <p>
+                    <span>
+                      {{ data.viewer.bio }}
+                    </span>
+                  </p>
+                  <p class="text-xs tracking-wide">
+                    I usually put all the projects I create on my Github
+                    profile. Sometimes they are larger applications and
+                    sometimes small test programs.
+                  </p>
+                  <p class="text-xs tracking-wide">
+                    I learn different technologies and try to use the acquired
+                    knowledge in code.
+                  </p>
+                  <p class="text-xs tracking-wide">
+                    In addition to programming fullstack applications, I also
+                    create larger or smaller scripts in Bash, Python and
+                    JavaScript.
+                  </p>
+                  <p class="text-xs tracking-wide">
+                    Currently, I have the most projects in JavaScript, but I
+                    also use popular libraries and frameworks. I learn Vue,
+                    React, Vuetify, Nuxt or Node all the time.
+                  </p>
+                  <p class="text-xs tracking-wide">
+                    Many projects are not very extensive or finished. I will
+                    highlight the most interesting and extensive ones on this
+                    page.
+                  </p>
+                  <p class="text-sm">
+                    It is amazing how many possibilities are offered by modern
+                    technologies!
+                  </p>
+                  <p>
+                    <span role="img" aria-label="inbox emoji" class="mr-1">
+                      📥️
+                    </span>
+                    DMs are open on
+                    <a
+                      class="transition-colors duration-200 text-blue-50 hover:text-blue-40"
+                      href="https://linkedin.com/in/"
+                    >
+                      LinkedIn
+                    </a>
+                    and
+                    <a
+                      class="transition-colors duration-200 text-blue-50 hover:text-blue-40"
+                      href="https://twitter.com/"
+                    >
+                      Twitter
+                    </a>
+                  </p>
+                </div>
               </div>
             </div>
           </div>
         </div>
+        <featured-repos-component :data="data" />
+        <recent-repos-component :data="data" />
       </div>
-      <featured-repos-component :data="data" />
-      <recent-repos-component :data="data" />
-    </div>
+    </template>
   </div>
 </template>
 
@@ -413,6 +418,66 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.loader,
+.loader::before,
+.loader::after {
+  background: #000;
+  -webkit-animation: load1 1s infinite ease-in-out;
+  animation: load1 1s infinite ease-in-out;
+  width: 1em;
+  height: 4em;
+}
+.loader {
+  color: #000;
+  text-indent: -9999em;
+  margin: 88px auto;
+  position: relative;
+  font-size: 11px;
+  -webkit-transform: translateZ(0);
+  -ms-transform: translateZ(0);
+  transform: translateZ(0);
+  -webkit-animation-delay: -0.16s;
+  animation-delay: -0.16s;
+}
+.loader::before,
+.loader::after {
+  position: absolute;
+  top: 0;
+  content: '';
+}
+.loader::before {
+  left: -1.5em;
+  -webkit-animation-delay: -0.32s;
+  animation-delay: -0.32s;
+}
+.loader::after {
+  left: 1.5em;
+}
+@-webkit-keyframes load1 {
+  0%,
+  80%,
+  100% {
+    box-shadow: 0 0;
+    height: 4em;
+  }
+  40% {
+    box-shadow: 0 -2em;
+    height: 5em;
+  }
+}
+@keyframes load1 {
+  0%,
+  80%,
+  100% {
+    box-shadow: 0 0;
+    height: 4em;
+  }
+  40% {
+    box-shadow: 0 -2em;
+    height: 5em;
+  }
+}
+
 .main-projects-container {
   margin-bottom: 140px;
 }
@@ -523,6 +588,8 @@ export default {
   display: flex;
   margin-left: auto;
   margin-right: auto;
+  padding-top: 60px;
+  padding-bottom: 60px;
 }
 .button-sect {
   min-width: 240px;
