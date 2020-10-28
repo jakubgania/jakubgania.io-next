ced879809444bbfc195b0e295bb8b19f863eb04e
jakubgania <contact@jakubgania.io>
2020-10-28 22:18:09 +0000

    initial resource page implementation

--- pages/_lang/resources/index.vue
+++ pages/_lang/resources/index.vue
@@ -1,5 +1,47 @@
 <template>
-  <div>
+  <div class="content-container">
     resources subpage
+    <div>
+      kategorie
+    </div>
+    <div>
+      lista
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: '',
+    }
+  },
+  head() {
+    return {
+      title: 'Jakub Gania Software | Zasoby',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Zasoby',
+        },
+        {
+          name: 'keywords',
+          content:
+            'programy, strony, portale, portale technologiczne, portale programistyczne, dokumentacje, projekty, blogi, artykuły, kanały youtube',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.content-container {
+  max-width: 800px;
+  width: 100%;
+  margin: auto;
+  margin-bottom: 100px;
+}
+</style>
